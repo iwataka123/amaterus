e97bfc41272b0532b1967ae19f91d2279cfd7c9c
iwataka123 <engineer.tk.hr@gmail.com>
2023-12-05 10:28:25 +0900

    修正

--- db/migrate/20231130054019_create_messages.rb
+++ db/migrate/20231130054019_create_messages.rb
@@ -2,8 +2,6 @@ class CreateMessages < ActiveRecord::Migration[7.0]
   def change
     create_table :messages do |t|
       t.text :content
-      t.references :room, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
