f04d0e7b873583f91584c05438039cef1d02025b
iwataka123 <engineer.tk.hr@gmail.com>
2023-12-05 16:43:33 +0900

    画像以外を送る機能

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,12 @@
 class Message < ApplicationRecord
+  has_one_attached :attached_file
+
   belongs_to :room
   belongs_to :user
 
-  validates :content, presence: true
+  validates :content, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.attached_file.attached?
+  end
 end
