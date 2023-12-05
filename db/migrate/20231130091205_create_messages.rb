229e717cce354275416bc25f61ea018b4453e360
iwataka123 <engineer.tk.hr@gmail.com>
2023-12-05 11:01:32 +0900

    修正２

--- db/migrate/20231130091205_create_messages.rb
+++ db/migrate/20231130091205_create_messages.rb
@@ -2,7 +2,12 @@ class CreateMessages < ActiveRecord::Migration[7.0]
   def change
     create_table :messages do |t|
       t.text :content
+      t.bigint :user_id, null:false
+      t.references :room, null:false, foreign_key:true
       t.timestamps
     end
+
+    add_foreign_key :messages, :users, column: :user_id
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_01_061416) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_30_091427) do
   create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -60,12 +60,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_01_061416) do
 
   create_table "messages", charset: "utf8mb4", force: :cascade do |t|
     t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.bigint "room_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["room_id"], name: "index_messages_on_room_id"
-    t.index ["user_id"], name: "index_messages_on_user_id"
+    t.index ["user_id"], name: "fk_rails_273a25a7a6"
   end
 
   create_table "room_users", charset: "utf8mb4", force: :cascade do |t|
