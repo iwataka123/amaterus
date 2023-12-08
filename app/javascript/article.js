4db7382193dae757726647e4772f843fc19fa659
iwataka123 <engineer.tk.hr@gmail.com>
2023-12-08 09:59:22 +0900

    JS修正

--- app/javascript/article.js
+++ app/javascript/article.js
@@ -2,7 +2,7 @@ window.addEventListener('turbo:load', function(){
   const articles = document.querySelectorAll(".article");
   articles.forEach(function(article) {
     article.addEventListener('mouseover', function() {
-      this.style.boxShadow = "0 10px 25px 0 rgba(0, 0, 0, 0.5)";
+      this.style.boxShadow = "0 10px 25px 0 rgba(0, 0, 0, 0.5), 0 2px 3px 0 rgba(0, 0, 0, 0.5)";
     });
     article.addEventListener('mouseout', function() {
       this.style.removeProperty("box-shadow");
