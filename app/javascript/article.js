60800b146a59b8374f14fe478681129b684ae6ea
iwataka123 <engineer.tk.hr@gmail.com>
2023-12-08 09:54:36 +0900

    JS修正

--- app/javascript/article.js
+++ app/javascript/article.js
@@ -1,11 +1,11 @@
 window.addEventListener('turbo:load', function(){
-  const articles = document.querySelectorAll(".article");   //querySelectorAll(".article")でarticleの要素をすべて取得
-  articles.forEach(function(article) {    //forEach(function(article)でarticlesの変数から要素を１つ１つ取り出し、articleという名前で扱えるようにする。
+  const articles = document.querySelectorAll(".article");
+  articles.forEach(function(article) {
     article.addEventListener('mouseover', function() {
-      this.setAttribute("style", "box-shadow: 0 10px 25px 0 rgba(0, 0, 0, 0.5);");
+      this.style.boxShadow = "0 10px 25px 0 rgba(0, 0, 0, 0.5)";
     });
     article.addEventListener('mouseout', function() {
-      this.removeAttribute("style", "box-shadow: 0 10px 25px 0 rgba(0, 0, 0, 0.5);");
+      this.style.removeProperty("box-shadow");
     });
   });
 });
